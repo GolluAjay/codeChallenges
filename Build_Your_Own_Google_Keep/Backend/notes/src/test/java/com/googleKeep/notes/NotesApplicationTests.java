2ed0be91949f357e79fbc187ff4db16f1b449cf2
ajay130 <gollu@revidd.com>
2024-01-20 14:26:22 +0530

    initial changes

---
+++ Build_Your_Own_Google_Keep/Backend/notes/src/test/java/com/googleKeep/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googleKeep.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
